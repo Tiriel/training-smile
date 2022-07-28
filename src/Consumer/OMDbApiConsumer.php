ca02094f0eb881db504eeb0e7f60beb6bee8aa55
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2022-07-28 12:03:56 +0200

    OMDB Consumer

---
+++ src/Consumer/OMDbApiConsumer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Consumer;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class OMDbApiConsumer
+{
+    public const MODE_ID = 'i';
+    public const MODE_TITLE = 't';
+
+    public function __construct(
+        private HttpClientInterface $omdbClient
+    ) {}
+
+    public function consume(string $type, string $value) : array
+    {
+        if (!\in_array($type, [self::MODE_ID, self::MODE_TITLE])) {
+            throw new \InvalidArgumentException(sprintf("Invalid mode provided for consumer : %s, or %s allowed, %s given",
+                self::MODE_ID, self::MODE_TITLE, $type));
+        }
+
+        return $this->omdbClient
+            ->request(Request::METHOD_GET, '', ['query' => [$type => $value]])
+            ->toArray();
+    }
+}
